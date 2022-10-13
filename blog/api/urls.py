897d86c4aaf3a6d1fdf2a636ec9defc11a14d3f9
FatihG34 <fthgnydn34@gmail.com>
2022-10-13 23:09:07 +0300

    like, view, comment operations added

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,14 +1,24 @@
 from .views import (
     CategoryView,
-    BlogPostView
+    BlogPostView,
+    BlogPostDetailView,
+    CommentView,
+    LikeView
 )
+from django.urls import path
 from rest_framework import routers
 
-router = routers.DefaultRouter()
-router.register('category', CategoryView)
-router.register('post', BlogPostView)
+# router = routers.DefaultRouter()
+# router.register('category', CategoryView)
+# router.register('post', BlogPostView)
 
 
 urlpatterns = [
+    path("category/", CategoryView.as_view()),
+    path("posts/", BlogPostView.as_view()),
+    path("like/", LikeView.as_view()),
+    path("posts/<str:slug>/", BlogPostDetailView.as_view()),
+    path("posts/<str:slug>/add_comment/", CommentView.as_view()),
 
-] + router.urls
+]
+# urlpatterns += router.urls
